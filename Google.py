bc8b0063faf2bff8ff498667226bc8170ef55560
AbelCodes247 <moltenabel@gmail.com>
2021-07-09 13:17:44 -0400

    commit all

--- Google.py
+++ Google.py
@@ -1,9 +1,18 @@
-class piglet:
-    pass
+#!/usr/bin/env python 3
 
-hamlet = piglet()
+def to_seconds(hours, minutes, seconds):
+    return hours*3600+minutes*60+seconds
 
+print("Welcome to this time converter")
 
-class piglet:
-    def speak(self):
-        print("oink oink")
+cont = "y"
+while(cont.lower() == "y"):
+    hours = int(input("Enter the number of hours: "))
+    minutes = int(input("Enter the number of minutes: "))
+    seconds = int(input("Enter the number of seconds: "))
+
+    print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
+    print()
+    cont = input("Do you want to do another conversion? [y to continue] ")
+
+print("Good bye!")
